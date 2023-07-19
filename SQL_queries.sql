3a5a248c14e57d7b299e544855e5e7cbb819de33
Zi-Stonga <62084056+Zi-Stonga@users.noreply.github.com>
2023-07-19 13:53:22 -0400

    Queries file

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -4,6 +4,22 @@ INSERT insurance_data bmi, bloodpressure, diabetic
 INTO Health_Related_Conditions;
 
 
+CREATE TABLE Claims (PatientID INT PRIMARY KEY,
+claim numeric);
+
+SELECT PatientID, claim
+FROM insurance_data;
+INSERT PatientID, claim
+INTO Claims;
+
+SELECT * FROM patient_basic_profile
+LEFT JOIN claims
+on patient_basic_profile.PatientID = claims.PatientID
+ORDER BY Claim DESC;
 
 
+SELECT * FROM health_related_conditions
+LEFT JOIN claims
+on health_related_conditions.PatientID = claims.PatientID
+ORDER BY Claim DESC;
 
